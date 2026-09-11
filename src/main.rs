8e62b63d451f32b776945319ece8b805d6c7acff
Anaru03 <dele22428@uvg.edu.gt>
2026-09-10 19:50:04 -0600

    Agregar intersección de rayos con cubo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+mod cube;
+mod ray;
+mod vector;
+
+use cube::Cube;
+use ray::Ray;
+use vector::Vec3;
+
 fn main() {
-    println!("Hello, world!");
+    // Crear un cubo centrado en el origen
+    let cube = Cube::new(Vec3::new(-1.0, -1.0, -1.0), Vec3::new(1.0, 1.0, 1.0));
+
+    // Crear un rayo frente al cubo
+    let ray = Ray::new(Vec3::new(0.0, 0.0, 5.0), Vec3::new(0.0, 0.0, -1.0));
+
+    println!("RayTracerCube");
+
+    if let Some(t) = cube.intersect(&ray) {
+        let hit_point = ray.at(t);
+        let normal = cube.normal_at(hit_point);
+
+        println!("El rayo golpeo el cubo");
+        println!("Distancia t: {}", t);
+        println!("Punto de impacto: {:?}", hit_point);
+        println!("Normal: {:?}", normal);
+    } else {
+        println!("El rayo no golpeo el cubo");
+    }
 }
